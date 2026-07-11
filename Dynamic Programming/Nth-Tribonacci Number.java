5dc563cddb5ad7e7e42544fed40e7dd096f04f08
akashgtub <akashunique982@gmail.com>
2026-07-11 09:52:45 +0530

    dp

--- Dynamic Programming/Nth-Tribonacci Number.java
+++ Dynamic Programming/Nth-Tribonacci Number.java
@@ -1,17 +1,18 @@
 class Solution {
-    public int fib(int n) {
+    public int tribonacci(int n) {
+        long[] dp=new long[n+1];
         if(n==0){
             return 0;
         }
-        if(n==1){
+        if(n==1||n==2){
             return 1;
         }
-        int[] dp=new int[n+1];
         dp[0]=0;
-         dp[1]=1;
-         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-         }
-         return dp[n];
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return (int)dp[n];
     }
 }
