d8452e96741a3e4b2f12e0af15d658e1dedd306e
akashgtub <akashunique982@gmail.com>
2026-07-13 10:07:52 +0530

    Dp

--- Dynamic Programming/Maximal Square.java
+++ Dynamic Programming/Maximal Square.java
@@ -1,3 +1,24 @@
-public class Maximal Square {
-    
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[][] dp=new int[m][n];
+        int max=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==0){
+                    dp[i][j]=matrix[i][j]-'0';
+                    continue;
+                }
+                if(matrix[i][j]=='1'){
+                    dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
+                }
+                else{
+                    dp[i][j]=0;
+                }
+                max=Math.max(max,dp[i][j]);
+            }
+        }
+        return max*max;
+    }
 }
