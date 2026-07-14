6c417f12ce97fcf4ee6f79aa552ff7b5fc23c698
akashgtub <akashunique982@gmail.com>
2026-07-14 14:41:27 +0530

    dp

--- Dynamic Programming/Letter Case permutation.java
+++ Dynamic Programming/Letter Case permutation.java
@@ -1,3 +1,23 @@
-public class Letter Case permutation {
-    
+class Solution {
+    static List<String> list;
+    static void solve(String s,int idx,String cur){
+        if(idx==s.length()){
+             list.add(cur);
+             return;
+        }
+        char ch=s.charAt(idx);
+        if(Character.isDigit(ch)){
+            solve(s,idx+1,cur+ch);
+        }
+        else{
+            solve(s,idx+1,cur+Character.toLowerCase(ch));
+            solve(s,idx+1,cur+Character.toUpperCase(ch));
+        }
+
+    }
+    public List<String> letterCasePermutation(String s) {
+        list=new ArrayList<>();
+        solve(s,0,"");
+        return list;
+    }
 }
