7fd40d00dd9db44e77dba0d46f34e1a53f9e76e4
akashgtub <akashunique982@gmail.com>
2026-07-14 14:38:35 +0530

    dp

--- Prefix/Range Sum Query Immutable.java
+++ Prefix/Range Sum Query Immutable.java
@@ -1,3 +1,24 @@
-public class Range Sum Query Immutable {
+class NumArray {
+  int[] dp;
+    public NumArray(int[] nums) {
+        int n=nums.length;
+        dp=new int[n];
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+         dp[i]=dp[i-1]+nums[i];
+        }
+    }
     
+    public int sumRange(int left, int right) {
+        if(left==0){
+            return dp[right];
+        }
+        return dp[right]-dp[left-1];
+    }
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
