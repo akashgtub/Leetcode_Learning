63d50b3578f02910824c49c3f424753c4093c169
akashgtub <akashunique982@gmail.com>
2026-07-19 20:56:41 +0530

    BFS

--- Breadh First Search (BFS)/Binary Tree Level Order Trversal.java
+++ Breadh First Search (BFS)/Binary Tree Level Order Trversal.java
@@ -1,5 +1,41 @@
-package Breadh First Search (BFS);
-
-public class Binary Tree Level Order Trversal {
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list=new ArrayList<>();
+        if(root==null){
+            return list;
+        }
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            List<Integer> cur=new ArrayList<>();
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode n=q.poll();
+                cur.add(n.val);
+                if(n.left!=null){
+                    q.add(n.left);
+                }
+                if(n.right!=null){
+                    q.add(n.right);
+                }
+            }
+            list.add(new ArrayList<>(cur));
+        }
+        return list;
+    }
 }
