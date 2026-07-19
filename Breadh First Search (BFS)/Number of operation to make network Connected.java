5ce2b0f3327f30213bc740696ed0f4c890db5292
akashgtub <akashunique982@gmail.com>
2026-07-19 20:54:31 +0530

    BFS

--- Breadh First Search (BFS)/Number of operation to make network Connected.java
+++ Breadh First Search (BFS)/Number of operation to make network Connected.java
@@ -1,5 +1,50 @@
-package Breadh First Search (BFS);
-
-public class Number of operation to make network Connected {
-    
+class Solution {
+    class DisJoint{
+        int[] parent;
+        int[] rank;
+        DisJoint(int n){
+            parent=new int[n];
+            rank=new int[n];
+            for(int i=0;i<n;i++){
+                parent[i]=i;
+            }
+        }
+        public int find(int x){
+            if(x!=parent[x]){
+                parent[x]=find(parent[x]);
+            }
+            return parent[x];
+        }
+        public  boolean union(int x,int y){
+            int rootX=find(x);
+            int rootY=find(y);
+            if(rootX==rootY){
+                return false;
+            }
+            if(rank[rootX]<rank[rootY]){
+                parent[rootX]=rootY;
+            }
+            else if(rank[rootX]>rank[rootY]){
+                parent[rootY]=rootX;
+            }
+            else{
+                parent[rootY]=rootX;
+                rank[rootX]++;
+            }
+            return true;
+        }
+    }
+    public int makeConnected(int n, int[][] connections) {
+        if(connections.length<n-1){
+            return -1;
+        }
+        int component=n;
+        DisJoint d=new DisJoint(n);
+        for(int[] e:connections){
+            if(d.union(e[0],e[1])){
+                component--;
+            }
+        }
+        return component-1;
+    }
 }
