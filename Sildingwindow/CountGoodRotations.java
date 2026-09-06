f1766e09cd4690f96216b382a35576ea76f36fa1
akashgtub <akashunique982@gmail.com>
2026-09-06 13:25:29 +0530

    sliding window

--- Sildingwindow/CountGoodRotations.java
+++ Sildingwindow/CountGoodRotations.java
@@ -2,16 +2,23 @@ class Solution {
     public int countGoodRotations(int[] nums) {
         int n=nums.length;
         int half=n/2;
-        int window=0;
-        int total=0;
+        long window=0;
+        long total=0;
         for(int num:nums){
             total+=num;
         }
         for(int i=0;i<half;i++){
             window+=nums[i];
         }
+        int count=0;
         for(int i=0;i<n;i++){
-            
+           long second=total-window;
+            if(window>second){
+                count++;
+            }
+            window-=nums[i];
+            window+=nums[(i+half)%n];
         }
+        return count;
     }
 }
