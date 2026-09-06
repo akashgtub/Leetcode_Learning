1289d546b424176a8deb92584b07272a8e4a4998
akashgtub <akashunique982@gmail.com>
2026-09-06 13:28:40 +0530

    sliding window

--- Arrays/CountRobotGroups.java
+++ Arrays/CountRobotGroups.java
@@ -1,3 +1,33 @@
-public @interface CountRobotGroups {
-    
+import java.util.*;
+
+class Solution {
+    public int countRobotGroups(int[] position, int[] speed, int distance) {
+        int n = position.length;
+        List<Integer> groups = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!groups.isEmpty() && position[i] - position[i - 1] <= distance) {
+                groups.set(groups.size() - 1, speed[i]);
+            } else {
+                groups.add(speed[i]);
+            }
+        }
+
+        int count = 0;
+        int frontSpeed = -1;
+
+        for (int i = groups.size() - 1; i >= 0; i--) {
+            int v = groups.get(i);
+
+            if (v <= frontSpeed) {
+                count++;
+                frontSpeed = v;
+            } else if (frontSpeed == -1) {
+                count++;
+                frontSpeed = v;
+            }
+        }
+
+        return count;
+    }
 }
