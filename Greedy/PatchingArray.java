d84bc5d1370a38728b3ad3ec704a2270cdaaeaa0
akashgtub <akashunique982@gmail.com>
2026-09-08 20:51:30 +0530

    greedy

--- Greedy/PatchingArray.java
+++ Greedy/PatchingArray.java
@@ -1,3 +1,17 @@
-public class PatchingArray {
-    
+class Solution {
+    public int minPatches(int[] arr, int n) {
+        int count=0,i=0;
+        long miss=1;
+        while(miss<=n){
+            if(i<arr.length&&arr[i]<=miss){
+                miss+=arr[i];
+                i++;
+            }
+            else{
+                miss+=miss;
+                count++;
+            }
+        }
+        return count;
+    }
 }
